ffb48b78fbfcf81f934300569cc67da27d7437ce
nazimkazim <nazik_111@mail.ru>
2020-05-09 14:19:23 +0600

    add sign in

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './routes'
+import Vue from 'vue';
+import App from './App.vue';
+import VueResource from 'vue-resource';
+import router from './routes';
+import store from './Store/Modules/store'
 
-import Button from './components/UI/Button.vue'
-import { MdCard } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
-Vue.component('app-button', Button)
-Vue.use(MdCard)
+import Button from './components/UI/Button.vue';
+import { MdCard } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+Vue.component('app-button', Button);
+
+
+Vue.use(MdCard);
+
+Vue.use(VueResource);
+Vue.http.options.root = '';
 
 
 new Vue({
   router,
+  store,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from './components/Home/Index.vue';
+import Signin from './components/Signin/Index'
 
 Vue.use(VueRouter);
 
 
 const routes = [
-  { path: '/', component: Home }
+  { path: '/', component: Home },
+  { path: '/signin', component: Signin }
 ];
 
 export default new VueRouter({
